14ed3fae21aaf6da1f1aa597610429988e9c8415
Joakim Palm <decent@live.se>
2016-12-28 23:36:03 +0100

    Corrected reading a short int with %hd instead of %d

--- amr15a.c
+++ amr15a.c
@@ -2,10 +2,10 @@
 int main()
 {
     short N, a, lucky = 0;
-    scanf("%d", &N);
+    scanf("%hd", &N);
     while(N--)
         {
-            scanf("%d", &a);
+            scanf("%hd", &a);
             (a%2 == 0)?(lucky++):(lucky--);
         }
     (lucky > 0)?(printf("READY FOR BATTLE")):(printf("NOT READY"));
